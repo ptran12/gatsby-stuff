5cd5fe5eedef421e4eb4273f3218258f558a5d75
ptran12 <phitran.722@gmail.com>
2020-10-30 17:27:20 -0500

    testing contenful

--- gatsby-stuff/src/test/Images.js
+++ gatsby-stuff/src/test/Images.js
@@ -15,7 +15,7 @@ const getImages = graphql`
     fluid: file(relativePath: { eq: "image-2.jpeg" }) {
       childImageSharp {
         fluid {
-          ...GatsbyImageSharpFluid
+          ...GatsbyImageSharpFluid_tracedSVG
         }
       }
     }
