b1747ea2d825e7858256a66940b5cd1160a944c0
ptran12 <phitran.722@gmail.com>
2020-10-30 18:22:32 -0500

    images rendering

--- gatsby-stuff/src/pages/products.js
+++ gatsby-stuff/src/pages/products.js
@@ -1,26 +1,46 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import styles from "../components/products.module.css"
+import Image from "gatsby-image"
+import { Link } from "gatsby"
 
-const products = () => {
+const ComponentName = ({ data }) => {
+  const {
+    allContentfulProduct: { nodes: products },
+  } = data
+  // console.log(products)
   return (
     <Layout>
-      <div className={styles.page}>
-        <h1>this is the product page</h1>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Feugiat
-          sed lectus vestibulum mattis ullamcorper velit. Erat pellentesque
-          adipiscing commodo elit at. Imperdiet proin fermentum leo vel orci.
-          Nunc id cursus metus aliquam eleifend mi in nulla. In massa tempor nec
-          feugiat nisl. Phasellus vestibulum lorem sed risus. Fermentum leo vel
-          orci porta non pulvinar neque. Nec feugiat nisl pretium fusce id velit
-          ut. Amet est placerat in egestas erat. In nisl nisi scelerisque eu.
-          Lectus quam id leo in vitae turpis.
-        </p>
-      </div>
+      <section className={styles.pages}>
+        {products.map(product => {
+          return (
+            <article key={product.id}>
+              <Image fluid={product.image.fluid} alt={product.title}></Image>
+            </article>
+          )
+        })}
+      </section>
     </Layout>
   )
 }
 
-export default products
+export const query = graphql`
+  {
+    allContentfulProduct {
+      nodes {
+        id
+        price
+        title
+        slug
+        image {
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+    }
+  }
+`
+
+export default ComponentName
